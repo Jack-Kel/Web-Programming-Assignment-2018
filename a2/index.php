7e3fc254e3d4e8a555addc999f5447e2359b589a
Jack Kelly <TheVault@Tamsins-MacBook-Air.local>
2018-09-01 11:26:09 +1000

    Added new styles as well as new pages.

--- a2/index.php
+++ a2/index.php
@@ -1,39 +1,46 @@
 <!DOCTYPE html>
 <html lang='en'>
-  <head>
-    <meta charset="utf-8">
-    <title>Assignment 2</title>
-    
-    <!-- Keep wireframe.css for debugging, add your css to style.css -->
-    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
-    <script src='../wireframe.js'></script>
-  </head>
 
-  <body>
+<head>
+	<meta charset="utf-8">
+	<title>Assignment 2</title>
+	<link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled/>
+	<link id='stylecss' type="text/css" rel="stylesheet" href="css/stylesheet.css" />
+	<script src='../wireframe.js'></script>
+</head>
 
-    <header>
-      <div>Put company logo and name here</div>
-    </header>
+<body>
 
-    <nav>
-      <div>Put website navigation links here</div>
-    </nav>
+	<nav id="navigation" class="topnav">
+		<a href="login.php">Login</a>
+		<a href="products.php">Products</a>
+		<a class="active" href="index.php">Home</a>
+	</nav>
 
-    <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-      </article>
-    </main>
+	<header id="masthead">
+		<article id='Logo'>
+			<img src='../../media/placeholder.png' alt='Logo' width="20%" height="auto" />
+		</article>
+		<div>Put company logo and name here</div>
+	</header>
 
-    <footer>
-      <div>&copy;<script>
-        document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here.</div>
-      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
-    </footer>
+	<main>
+		<article id='Website Under Construction'>
+			<!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
+			<img src='../../media/website-under-construction.png' alt='Website Under Construction' />
+		</article>
+	</main>
 
-  </body>
+	<footer>
+		<div>&copy;
+			<script>
+				document.write(new Date().getFullYear());
+
+			</script> Jack, s3707738 and DollarMerchStore(TM).</div>
+		<div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+		<div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+	</footer>
+
+</body>
+
 </html>
